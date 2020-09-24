289d3247b751732045d2d2cc5a4943b510ff3c1d
Alex <avseenkov_a@mail.ru>
2020-09-24 19:03:32 +0200

    Добавил мета тег

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,26 @@ import Services from '../Components/Services';
 import Contacts from '../Components/Contacts';
 import Send from '../Components/Send';
 import Jumbotron from '../Components/Jumbotron';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
-		<Layout>
-			<Jumbotron />
-			<AboutUs />
-			<Services />
-			<Contacts />
-			<Send />
-		</Layout>
+		<React.Fragment>
+			<Head>
+				<title>Пескостройная обработка в СПб и ЛО</title>
+				<meta
+					key="description"
+					name="description"
+					content="Пескоструная обработка в Санкт-Петербурге и ЛО. Сварка кузова любой сложности в СПб"
+				/>
+			</Head>
+			<Layout>
+				<Jumbotron />
+				<AboutUs />
+				<Services />
+				<Contacts />
+				<Send />
+			</Layout>
+		</React.Fragment>
 	);
 }
